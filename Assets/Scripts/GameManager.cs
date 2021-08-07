142fbbf18056e322cc54e34ba023ba582be8ba56
Eric Felts <echristianfelts@gmail.com>
2021-08-07 09:44:11 -0700

    Added Load Areana Method.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,5 +41,21 @@ namespace Com.MyCompany.MyGame
 
 
         #endregion
+
+        #region Private Methods
+
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+
+        #endregion
     }
 }
